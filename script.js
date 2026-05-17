18a6cdb06ecd33b732d8100d018554f8bfddc491
ValentinSTEBAN <valentinstebancours@gmail.com>
2026-05-17 17:29:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const CVBtn = document.getElementById('CVPdf');
+
+CVBtn.addEventListener('click', function() {
+    const pdfUrl = 'cv_test.pdf';
+    const fileName = 'cv_test.pdf';
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
+
+
+const GitHubBTN = document.getElementById('GitHub');
+
+GitHubBTN.addEventListener('click', function() {
+    window.open('https://github.com/ValentinSTEBAN');
+});
