d543998917999bcf0201c971d9a9338f68482fd3
ValentinSTEBAN <valentinstebancours@gmail.com>
2026-05-23 21:51:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,9 @@ const GitHubBTN = document.getElementById('GitHub');
 GitHubBTN.addEventListener('click', function() {
     window.open('https://github.com/ValentinSTEBAN');
 });
+
+const LinkedinBTN = document.getElementById('Linkedin');
+
+LinkedinBTN.addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/valentin-steban-42891540a/');
+});
